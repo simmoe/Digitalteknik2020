b1140ef6c7964b652f1ce42062e47a2cfdfddf23
simmoe <simmoe@gmail.com>
2020-11-18 14:42:34 +0100

    android ssl mess

--- hybrid-template/app/App.svelte
+++ hybrid-template/app/App.svelte
@@ -1,23 +1,16 @@
 <script>
-    let message = "Blank Svelte Native App"
+let message = "Blank Svelte Native App"
 </script>
 
 <page>
     <actionBar title="Svelte Native App"/>
     <gridLayout>
-        <label class="text" text="My new app" />
+        <webView src="http://ddubaselineserver20.it.nextkbh.dk"/>     
     </gridLayout>
-
-
 </page>
 
 
 <style>
-    .text{
-        font-size: 24;
-        color:lightblue;
-        text-align: center;
-    }
 </style>
 
 
