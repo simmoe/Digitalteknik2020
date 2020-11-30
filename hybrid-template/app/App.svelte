c9990e26d692ea3ed5100e331e9a33e555bc394a
simmoe <simmoe@gmail.com>
2020-11-30 13:06:41 +0100

    various

--- hybrid-template/app/App.svelte
+++ hybrid-template/app/App.svelte
@@ -1,30 +1,14 @@
 <script lang="typescript">
-    import { isAndroid, isIOS, device, screen } from "tns-core-modules/platform"
-    
     let message = "Blank Svelte Native App"
-    const settings = (o) => {
-        console.log(o, "Hejsa")
-
-    // let webView = <webView>args.object;
-
-    if (isAndroid) {
-        setTimeout(() => {
-           let androidWebView = webView.nativeView; // android.webkit.WebView
-           androidWebView.getSettings().setMediaPlaybackRequiresUserGesture(false);
-        }, 100);
-    }
-}
-
 </script>
 
 <page>
-    <actionBar title="Svelte Native App"/>
-    <gridLayout>
-        <webView on:loadFinished={settings} src='https://ddutemplateserver.elev.nextkbh.dk/wordpress/' />
+    <actionBar title="Hybrid app" />
+    <gridLayout verticalAlignment="center">
+        <label text={message} class="info text-center"/>
     </gridLayout>
 </page>
 
- 
 <style>
     .info {
         font-size: 20;
