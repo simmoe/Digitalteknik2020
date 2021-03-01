0855b894e987c51a7cbf525fdf04dc376f9dd277
simmoe <simmoe@gmail.com>
2021-03-01 19:55:46 +0100

    back to zero

--- mqtt_p5_sockets/sketch.js
+++ mqtt_p5_sockets/sketch.js
@@ -1,36 +1,15 @@
-let client 
-let light
 
 function setup() {
-  createCanvas(displayWidth, displayHeight)
-  mqttInit()
-  client.publish('m5/status', 'Connection from webpage')
-  createElement('p', 'Lys sensor får data fra M5 over MQTT')
-  light = createElement('h1', '0')
-  client.subscribe('m5/sensor/light')
-  client.on('message', (topic, message, packet) => {
-    console.log('Received Message: ' + message.toString() + '\nOn topic: ' + topic)
-    if(topic == 'm5/sensor/light'){
-      light.html(message)
-    }
-  })
 }
 function draw() {
-  clear() 
-  background(153)
-  let val = map(light.html(), 0, 1000, 0, 255)
-  noStroke()
-  fill(255,0,0,val)
-  ellipse(displayWidth/2, 400, 400)
 }
 
 
 
-
+ 
 
 
 const mqttInit = () => {
-  //opret unikt id 
   const clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
   const host = 'wss://test.mosquitto.org:8081'
 
@@ -67,6 +46,9 @@ const mqttInit = () => {
     console.log('Client connected:' + clientId)
   })
 
+  client.on('message', (topic, message, packet) => {
+    console.log('Received Message: ' + message.toString() + '\nOn topic: ' + topic)
+  })
 
   client.on('close', () => {
     console.log(clientId + ' disconnected')
